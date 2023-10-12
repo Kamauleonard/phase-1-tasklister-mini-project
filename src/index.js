e1baabeedc488f59787f5f17d9f08e955754cee0
Kamauleonard <leonardmwangi2012@gmail.com>
2023-10-12 16:33:47 +0300

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+    const form = document.getElementById('create-task-form');
+    const taskList = document.getElementById('tasks');
+
+    form.addEventListener('submit', function(event) {
+        event.preventDefault(); 
+
+        
+        const taskInput = document.getElementById('new-task-description');
+        const taskText = taskInput.value;
+
+      
+        const taskItem = document.createElement('li');
+        taskItem.textContent = taskText;
+
+     
+        const deleteBtn = document.createElement('span');
+        deleteBtn.textContent = ' -';
+        deleteBtn.className = 'delete-btn';
+
+      
+        taskItem.appendChild(deleteBtn);
+
+       
+        taskList.appendChild(taskItem);
+
+       
+        taskInput.value = '';
+    });
+
+    
+    taskList.addEventListener('click', function(event) {
+        if (event.target.classList.contains('delete-btn')) {
+            const taskItem = event.target.parentElement;
+            taskList.removeChild(taskItem);
+        }
+    });
 });
